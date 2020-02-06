bcf8ee50df62494780154b8a9548c3e3daf22d9e
Caroll1899 <diahancaroll@hotmail.com>
2020-02-06 21:24:18 +0000

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+
+    url = 'https://api.reddit.com/r/{}?sort=hot&limit=10'.format(subreddit)
+
+    headers = {
+        'User-Agent': 'My User Agent 1.0'
+    }
+
+    req = requests.get(url, headers=headers)
+
+    if req.status_code == 200:
+        req_json = req.json()
+
+        data = req_json.get('data')
+        children = data.get('children')
+        for i in children:
+            title = i.get('data').get('title')
+
+            print(title)
